b077c3706925129f528b029912c1e4e88ec97ec5
Virgil-Avram <avraam.virgil@yahoo.com>
2020-07-23 10:17:34 +0200

    Display loader to the user while fetching the events data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h1>Amsterdam Events</h1>
+    <section v-if="loading">
+      <p>Loading...</p>
+    </section>
     <ul v-if="events.data">
       <li v-for="eventItem in events.data._embedded.events" :key="eventItem.id">
         <h3>{{eventItem.name}}</h3>
@@ -23,10 +26,13 @@ export default class Home extends Vue {
 
   //Set the intial data state
   private events: object = {}
+  private loading: boolean = null
 
   //Make get request for 10 events in Amsterdam
   //to ticket master api after the component mounts
   mounted() {
+    //Display loader while fetching the events data from the api
+    this.loading = true
 
     axios.get('https://app.ticketmaster.com/discovery/v2/events.json?apikey=TROvAEVWbwaLGs6P8wsutq4jzMGkwQky&city=Amsterdam&page=1&size=10')
      .then((response: object)  => {
@@ -37,6 +43,9 @@ export default class Home extends Vue {
        console.log(error)
        }
      )
+     .finally(() => {
+       this.loading = false
+     })
   }
 }
 </script>
