d5f8081268638e20f452da50568407f403484dc4
Virgil-Avram <avraam.virgil@yahoo.com>
2020-07-28 18:01:33 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,18 @@
       </select>
     </div>
     <router-view />
+    <Footer />
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 @Component({
   components: {
-    Header
+    Header,
+    Footer
   }
 })
 export default class App extends Vue {
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="footer-container">
+    <footer>
+      <router-link to="/">Home</router-link>
+      <router-link to="/randomizer">Randomizer</router-link>
+      <p><slot>©</slot> 2019 Virgil Avram</p>
+      <p>Powered by the TicketMaster Api</p>
+    </footer>
+  </div>
+</template>
