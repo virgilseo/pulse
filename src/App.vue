b4aefb161b9b59d03e0cfb40d640bc2d48028351
Virgil-Avram <avraam.virgil@yahoo.com>
2020-07-29 15:13:25 +0200

    Show categories section only on the home page

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/randomizer">Randomizer</router-link> |
-      <label for="categories">Categories </label>
-      <select class="select" name="categories" @change="changeCategorie" v-model="categorie">
-        <option value="" disabled>select</option>
-        <option value="music">music</option>
-        <option value="Arts & Theatre">arts and theatre</option>
-        <option value="Miscellaneous">other</option>
-      </select>
+      <div v-if="currentPage === 'Home'" class="categories-container">
+        <label for="categories">Categories </label>
+        <select class="select" name="categories" @change="changeCategorie" v-model="categorie">
+          <option value="" disabled>select</option>
+          <option value="music">music</option>
+          <option value="Arts & Theatre">arts and theatre</option>
+          <option value="Miscellaneous">other</option>
+        </select>
+      </div>
     </div>
     <router-view />
     <Footer />
@@ -34,6 +36,9 @@ export default class App extends Vue {
   private changeCategorie(): void {
     console.log(this.categorie)
     this.$store.commit("updateCategorie", this.categorie)
+  }
+  get currentPage(): string {
+    return this.$route.name || '';
   }
 }
 </script>
