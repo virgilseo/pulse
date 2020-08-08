9cc3b26ddca6a5061ab09760521b1a382c67a0cb
Virgil-Avram <avraam.virgil@yahoo.com>
2020-08-08 16:50:35 +0200

    Style the categories section

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="categories">
+  <div class="categories-container">
     <div class="menu-container">
-      <span>Categories</span>
-      <i @click="toggleDropDown" class="material-icons">arrow_drop_down</i>
+      <span class="categories-label">Categories</span>
+      <i @click="toggleDropDown" class="material-icons dropdown-icon">
+        <span v-if="!showCategories">expand_more</span>
+        <span v-if="showCategories">expand_less</span>
+      </i>
     </div>
     <div v-if="showCategories" class="categorie-dropdown">
       <ul>
-        <li @click="changeCategorie(categorie)" v-for="categorie in categories" :key="categorie.index">
+        <li class="categorie" @click="changeCategorie(categorie)" v-for="categorie in categories" :key="categorie.index">
           {{ categorie.name }}
+          <i v-if="categorie.isSelected" class="material-icons check-icon">check</i>
         </li>
       </ul>
     </div>
@@ -25,7 +29,19 @@ export default class Categories extends Vue {
     { name: "music", isSelected: true },
     { name: "arts & theatre", isSelected: false },
     { name: "miscellaneous", isSelected: false },
-  ]
+  ];
+
+  mounted() {
+    //Perssist categorie selection icon when the user navigates
+    //back to the home page
+    this.categories.forEach(item => {
+      item.isSelected = false;
+
+      if(item.name === this.currentCategorie) {
+        item.isSelected = true;
+      }
+    });
+  }
 
   //Toggle categories menu on user input
   private toggleDropDown(): void {
@@ -33,8 +49,15 @@ export default class Categories extends Vue {
   }
   //Change categories on user input
   private changeCategorie(categorie: object): void {
-    !categorie.isSelected ? categorie.selected = true : undefined;
+    this.categories.forEach(item => {
+      item.isSelected = false
+    });
+    categorie.isSelected = true;
     this.$store.commit("home/updateCategorie", categorie.name);
+  }
+  //Get current categorie selection from the store
+  get currentCategorie(): string {
+    return this.$store.getters["home/categorie"];
   }
 }
 
